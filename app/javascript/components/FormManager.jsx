6d4fda947bc86818fd6da773f6c3205010ca991b
Alvin Crespo <alvin.crespo@gmail.com>
2019-11-15 14:41:44 -0500

    create form renderer

--- app/javascript/components/FormManager.jsx
+++ app/javascript/components/FormManager.jsx
@@ -1,14 +1,58 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
-const FormManager = () => {
-  const [, setDisplayForm] = useState(false);
+const NewUserForm = ({ onSave, onCancel }) => {
+  return (
+    <form>
+      <div class="field">
+        <label htmlFor="user_name">Name</label>
+        <input type="text" id="user_name" />
+      </div>
+      <div class="field">
+        <label htmlFor="user_dob">Birthdate</label>
+        <input type="date" id="user_dob" />
+      </div>
+      <div class="actions">
+        <button type="button">Cancel</button>
+        <button type="submit">Create User</button>
+      </div>
+    </form>
+  );
+};
 
-  return ReactDOM.createPortal(
-    <button type="button" onClick={() => setDisplayForm(true)}>
+const FormAction = ({ onClick }) =>
+  ReactDOM.createPortal(
+    <button type="button" onClick={onClick}>
       New User
     </button>,
     document.getElementById("portal-action")
+  );
+
+const FormRenderer = ({ display, onSave, onCancel }) => {
+  if (!display) return null;
+
+  return ReactDOM.createPortal(
+    <NewUserForm onSave={onSave} onCancel={onCancel} />,
+    document.getElementById("portal-form")
+  );
+};
+
+const FormManager = () => {
+  const [displayForm, setDisplayForm] = useState(false);
+
+  const onSave = () => {
+    // do something
+  };
+
+  const onCancel = () => {
+    // do something
+  };
+
+  return (
+    <>
+      <FormAction onClick={() => setDisplayForm(true)} />
+      <FormRenderer display={displayForm} onSave={onSave} onCancel={onCancel} />
+    </>
   );
 };
 
